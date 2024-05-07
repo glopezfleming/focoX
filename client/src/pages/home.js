5f11b029b1657041e122114eb07c2e3813e5ef0c
glopezfleming <gustavo.lopez-fleming.jr.26@dartmouth.edu>
2024-05-07 14:52:11 -0400

    saved-recipes UI

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,9 +1,78 @@
 import React from 'react'
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import useGetUserID from "../hooks/useGetUserID";
 
-const home = () => {
+
+const Home = () => {
+  const [recipes, setRecipes] = useState([])
+  const userID = useGetUserID();
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  useEffect(() =>{
+    const fetchRecipe = async () => {
+      try{
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data)
+
+      }catch(err){
+        console.log(err)
+      }
+    }
+
+    const fetchSavedRecipe = async () => {
+      try{
+        const response = await axios.get(`http://localhost:3001/recipes/savedRecipes/ids/${userID}`);
+        setSavedRecipes(response.data.savedRecipes)
+      }catch(err){
+        console.log(err)
+      }
+    }
+
+
+
+
+    fetchRecipe();
+    fetchSavedRecipe();
+
+  }, []);
+
+  const saveRecipe = async (recipeID) =>{
+    try{
+      const response = await axios.put("http://localhost:3001/recipes", {recipeID, userID});
+      setSavedRecipes(response.data.savedRecipes)
+
+    }catch(err){
+      console.log(err)
+    }
+  } 
+
+  const isRecipeSaved = (id) => savedRecipes.includes(id);
+
+
   return (
-    <div>home</div>
+
+    <div>
+      <h2>Recipes</h2>
+      <ul>
+        {recipes.map((recipe)=>(
+          <li key = {recipe._id}>
+            {savedRecipes.includes(recipe._id) && <h1>Saved</h1>}
+            <div>
+              <h2>{recipe.name}</h2>
+              <button onClick = {() => saveRecipe(recipe._id)} disabled = {isRecipeSaved(recipe._id)}>
+                {isRecipeSaved(recipe._id) ? "Saved" : "Save"}
+              </button>
+            </div>
+            <div>
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src = {recipe.imageURL} alt = {recipe.name}></img>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export default home
+export default Home
--- client/src/pages/saved-recipes.js
+++ client/src/pages/saved-recipes.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import useGetUserID from "../hooks/useGetUserID";
+
 
 const SavedRecipes = () => {
+  const [savedRecipes, setSavedRecipes] = useState([])
+  const userID = useGetUserID();
+
+
+  useEffect(() =>{
+
+
+    const fetchSavedRecipe = async () => {
+      try{
+        const response = await axios.get(`http://localhost:3001/recipes/savedRecipes/${userID}`);
+        setSavedRecipes(response.data.savedRecipes)
+      }catch(err){
+        console.log(err)
+      }
+    }
+
+    fetchSavedRecipe();
+
+  }, []);
+
+
+
   return (
-    <div>SavedRecipes</div>
+
+    <div>
+      <h2>Saved Recipes</h2>
+      <ul>
+        {savedRecipes.map((recipe)=>(
+          <li key = {recipe._id}>
+            {savedRecipes.includes(recipe._id) && <h1>Saved</h1>}
+            <div>
+              <h2>{recipe.name}</h2>
+        
+            </div>
+            <div>
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src = {recipe.imageURL} alt = {recipe.name}></img>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
